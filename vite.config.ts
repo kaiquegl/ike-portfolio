97ae2822a350297f92a6d1057e1293a6052b8d65
Kaique Lima <kaique@leanwork.com.br>
2026-02-23 17:22:36 -0300

    fix: remove nitro config from build

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig, type PluginOption } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
+  build: {
+    outDir: "dist/client"
+  },
   plugins: [
     devtools(),
-    nitro(),
+    // nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"]
